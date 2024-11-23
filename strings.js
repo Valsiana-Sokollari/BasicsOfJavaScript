b9eb0c5b01fb14e4bc8085b62b4992cb4e678ae2
Valsiana-Sokollari <vsokollari@gmail.com>
2024-11-23 17:11:54 +0100

    Ushtrime te reja me stringa

--- strings.js
+++ strings.js
@@ -113,3 +113,163 @@ function capitalize(string){
 
 console.log("Stringa origjinale: "+ lowerCaseString);
 console.log("Stringa ku cdo germe e pare eshte kapitale: "+ capitalize(lowerCaseString));
+
+//Ush 8 Shkruaj nje funksion ne JavaScript qe merr si parameter nje stringe qe permban edhe shkronja 
+//te vogla edhe te medha dhe konverton shkronjat e vogla ne te medha dhe ato te medha ne te vogla
+
+originalString = "AbdFGhjkR";
+
+function invertCase(string){
+    let invertedString='';
+    for(let index =0;index<string.length;index++){
+        if(string.charAt(index) ==string.charAt(index).toUpperCase()){
+            invertedString+=string.charAt(index).toLowerCase()
+        }
+        else{
+            invertedString+=string.charAt(index).toUpperCase()
+        }
+    }
+    return invertedString;
+}
+
+console.log("Versioni i invertuar i stringes: "+originalString+" eshte "+invertCase(originalString));
+
+//Ush9 Shkruaj nje funksion qe e konverton nje stringe ne camelCase
+
+originalString = "I am valuable no matter what"
+
+function camelize(string){
+    let words = string.split(" ");
+    let capitalizedWord='';
+    let camelizedVersion='';
+    for(let i=0;i<words.length;i++){
+        let firstLetter = words[i].charAt(0);
+        if(firstLetter==firstLetter.toLowerCase()){
+            firstLetter = firstLetter.toUpperCase();
+        }
+       
+
+        capitalizedWord = firstLetter+words[i].substring(1);
+        
+        camelizedVersion+=capitalizedWord;
+    }
+
+    return camelizedVersion;
+}
+
+console.log("Versioni camelCase i stringut: "+ originalString+ " eshte: "+ camelize(originalString));
+
+//Ush10 Shkruaj nje funksion qe vendos nje stringe brenda nje stringu ne nje pozicion te caktuar. By default pozicioni eshte 1
+// Pyetje : Si ta kontrollojme nese nje argument mungon apo jo?
+let mainString = "My name is Valsiana";
+let nestedString = "Sokollari";
+
+function insertString(mainString,nestedString,pozition){
+     let part1 = mainString.substring(0,pozition);
+     let part2 = mainString.substring(pozition);
+
+     let result = part1+nestedString+part2;
+     return result;
+
+}
+
+console.log("Stringa: "+mainString+" pas nderthurjes me "+nestedString+" eshte "+ insertString(mainString,nestedString,11));
+
+//Ush11 Shkruaj nje funksion ne JavaScript qe formaton nje numer ne nje stringe te lexueshem nga syri njerezor
+//P.sh 1st 2nd 43rd
+//Rregulli numrat qe mbarojne me 1 marrin st
+//numrat qe mbarojne me 2 marrin nd
+//Numrat qe mbarojne me 3 marrin rd
+//Vec 11 12 dhe 13 bejne perjashtim pasi marrin th dhe gjithe numrat e tjere marrin th
+
+let number = 56;
+
+function formatNumber(number){
+    let formattedNumber;
+    if(number===11||number===12||number===13){
+        formattedNumber=number+="th"
+    }
+    else {
+        stringVersion = new String(number);
+        if(stringVersion.charAt(stringVersion.length-1)==="1")
+            formattedNumber= number+"st"
+        else if(stringVersion.charAt(stringVersion.length-1)==="2")
+            formattedNumber= number+"nd"
+         else if(stringVersion.charAt(stringVersion.length-1)==="3")
+            formattedNumber= number+"rd"
+        else
+            formattedNumber= number+"th"
+    }
+
+    return formattedNumber;
+}
+
+console.log("Numri: "+number+" i formatuar eshte: "+formatNumber(number));
+
+//Ush12 Shkruaj nje funksion ne JS per te shkurtuar nje stringe nese eshte me e gjate 
+//se nje numer specifik karakteresh. Stringat e shkurtuara do te mbarojne me karakteret ...
+//by default por nese karakteri specifikohet do te mbaroje me ate karakter
+
+let stringToTruncate ="I am learning JavaScript";
+
+function truncateString(string,number,character){
+    let truncatedString='';
+    if(string.length<number){
+        truncatedString=string
+    }
+    else if(character===undefined){
+        truncatedString = string.substring(0,number)+"..."
+    }
+    else {
+        truncatedString=string.substring(0,number)+character?.repeat(3)
+    }
+
+    return truncatedString;
+
+}
+
+console.log("Stringa e shkurtuar eshte: "+truncateString(stringToTruncate,5,"&"));
+
+//Ush13 Shkruaj nje funksion qe ndan nje stringe neper pjese me gjatesi te dhene. Nese gjatesia
+//nuk jepet stringa nuk ndahet ne nenpjese
+
+let stringToChunk = "I am writing some code in JS"
+
+function chunkString(string,length){
+    let resultedChunks =[];
+    if(length===0){
+        resultedChunks.push(string)
+    }
+    else {
+         let index =0
+         while(index<string.length){
+            resultedChunks.push(string.substring(index,index+length))
+            index+=length
+         }
+    }
+    return resultedChunks;
+
+}
+
+console.log("Stringa e ndare eshte: "+ chunkString(stringToChunk,2));
+
+//Ush14 Shkruaj nje funksion ne JS qe numeron numrin e substringave ne nje stringe
+
+originalString="The quick brown fox jumps over the lazy dog"
+
+function countSubStrings(string, substring) {
+    let count = 0;
+    let index = string.toLowerCase().indexOf(substring.toLowerCase()); // Kontroll për substring case-insensitive
+    while (index !== -1) {
+        count++;
+        index = string.toLowerCase().indexOf(substring.toLowerCase(), index + substring.length);
+    }
+    return count;
+}
+
+
+const substring = "the";
+
+console.log("Numri i herëve që substringa \"" + substring + 
+            "\" shfaqet në stringun \"" + originalString + "\" është: " + 
+            countSubStrings(originalString, substring));
